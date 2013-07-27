5feec60d6bda73126cba74ec0152f6b37368cc0f
Koushik Dutta <koushd@gmail.com>
2013-07-26 21:35:23 -0700

    WIP: 4.3 fixes. DO NOT SUBMIT.
    Change-Id: I2bdc7ef61dc17430a878e13dd098e6d0c004ddb2

--- Superuser/jni/su/su.c
+++ Superuser/jni/su/su.c
@@ -32,9 +32,14 @@
 #include <sys/stat.h>
 #include <stdarg.h>
 #include <sys/types.h>
+#include <pthread.h>
 
 #include "su.h"
 #include "utils.h"
+
+static int is_daemon = 0;
+static int daemon_from_uid = 0;
+static int daemon_from_pid = 0;
 
 unsigned get_shell_uid() {
   struct passwd* ppwd = getpwnam("shell");
@@ -373,7 +378,7 @@ do {                                                \
     }                                               \
 } while (0)
 
-#define write_string(fd, name, data)        \
+#define write_string_data(fd, name, data)        \
 do {                                        \
     write_data(fd, name, strlen(name));     \
     write_data(fd, data, strlen(data));     \
@@ -384,19 +389,19 @@ do {                                        \
 do {                                        \
     char buf[16];                           \
     snprintf(buf, sizeof(buf), "%d", data); \
-    write_string(fd, name, buf);            \
+    write_string_data(fd, name, buf);            \
 } while (0)
 
     write_token(fd, "version", PROTO_VERSION);
     write_token(fd, "binary.version", VERSION_CODE);
     write_token(fd, "pid", ctx->from.pid);
-    write_string(fd, "from.name", ctx->from.name);
-    write_string(fd, "to.name", ctx->to.name);
+    write_string_data(fd, "from.name", ctx->from.name);
+    write_string_data(fd, "to.name", ctx->to.name);
     write_token(fd, "from.uid", ctx->from.uid);
     write_token(fd, "to.uid", ctx->to.uid);
-    write_string(fd, "from.bin", ctx->from.bin);
+    write_string_data(fd, "from.bin", ctx->from.bin);
     // TODO: Fix issue where not using -c does not result a in a command
-    write_string(fd, "command", get_command(&ctx->to));
+    write_string_data(fd, "command", get_command(&ctx->to));
     write_token(fd, "eof", PROTO_VERSION);
     return 0;
 }
@@ -586,7 +591,306 @@ int access_disabled(const struct su_initiator *from) {
     return 0;
 }
 
+static int read_int(int fd) {
+    int val;
+    int len = read(fd, &val, sizeof(int));
+    if (len < sizeof(int)) {
+        LOGE("unable to read expected amount");
+        exit(-1);
+    }
+    return val;
+}
+
+static void write_int(int fd, int val) {
+    int written = write(fd, &val, sizeof(int));
+    if (written != sizeof(int)) {
+        PLOGE("unable to write expected amount");
+        exit(-1);
+    }
+}
+
+static char* read_string(int fd) {
+    int len = read_int(fd);
+    char* val = malloc(sizeof(char) * len + 1);
+    val[len] = '\0';
+    int amount = read(fd, val, len);
+    if (amount != len) {
+        LOGE("unable to read expected amount");
+        exit(-1);
+    }
+    return val;
+}
+
+static void write_string(int fd, char* val) {
+    int len = strlen(val);
+    write_int(fd, len);
+    int written = write(fd, val, len);
+    if (written != len) {
+        PLOGE("unable to write expected amount");
+        exit(-1);
+    }
+}
+
+static int setup_daemon_child(int pid, int argc, char** argv) {
+    char outfile[PATH_MAX];
+    char infile[PATH_MAX];
+    sprintf(outfile, "%s/%d.stdout", REQUESTOR_DAEMON_PATH, pid);
+    sprintf(infile, "%s/%d.stdin", REQUESTOR_DAEMON_PATH, pid);
+    LOGD("child infile: %s", infile);
+    LOGD("child outfile: %s", outfile);
+
+    // as the daemon, we read from out, and write to in
+    int outfd = open(outfile, O_WRONLY);
+    if (outfd <= 0) {
+        PLOGE("outfd");
+        exit(-1);
+    }
+    int infd = open(infile, O_RDONLY);
+    if (infd <= 0) {
+        PLOGE("infd");
+        exit(-1);
+    }
+
+    // remote out is daemon stdin
+    if (-1 == dup2(outfd, STDOUT_FILENO)) {
+        PLOGE("dup2 child outfd");
+        exit(-1);
+    }
+
+    // remote in is daemon stdout
+    if (-1 == dup2(infd, STDIN_FILENO)) {
+        PLOGE("dup2 child infd");
+        exit(-1);
+    }
+    return main(argc, argv);
+}
+
+static int daemon_accept(int fd) {
+    is_daemon = 1;
+    int pid = read_int(fd);
+    LOGD("remote pid: %d", pid);
+    daemon_from_uid = read_int(fd);
+    LOGD("remote uid: %d", daemon_from_uid);
+    daemon_from_pid = read_int(fd);
+    LOGD("remote req pid: %d", daemon_from_pid);
+    int argc = read_int(fd);
+    LOGD("remote args: %d", argc);
+    char** argv = (char**)malloc(sizeof(char*) * (argc + 1));
+    argv[argc] = NULL;
+    int i;
+    for (i = 0; i < argc; i++) {
+        argv[i] = read_string(fd);
+        LOGD("arg: %s", argv[i]);
+    }
+
+    int code;
+    // now fork and run main, watch for the child pid exit, and send that
+    // across the control channel as the response.
+    int child = fork();
+    if (child < 0) {
+        code = child;
+        goto done;
+    }
+
+    // if this is the child, open the fifo streams
+    // and dup2 them with stdin/stdout, and run main, which execs
+    // the target.
+    if (child == 0) {
+        close(fd);
+        LOGD("main!");
+
+        return setup_daemon_child(pid, argc, argv);
+    }
+    
+    // otherwise, wait for the child to exit, and send the exit code
+    // across the wire.
+    int status;
+    LOGD("waiting for child exit");
+    if (waitpid(child, &status, 0) > 0) {
+        code = WEXITSTATUS(status);
+    }
+    else {
+        code = -1;
+    }
+
+done:
+    write(fd, &code, sizeof(int));
+    close(fd);
+    return code;
+}
+
+static int run_daemon() {
+    LOGD("starting su daemon");
+    int fd;
+    struct sockaddr_un sun;
+
+    fd = socket(AF_LOCAL, SOCK_STREAM, 0);
+    if (fd < 0) {
+        PLOGE("socket");
+        return -1;
+    }
+
+    memset(&sun, 0, sizeof(sun));
+    sun.sun_family = AF_LOCAL;
+    sprintf(sun.sun_path, "%s/server", REQUESTOR_DAEMON_PATH);
+
+    /*
+     * Delete the socket to protect from situations when
+     * something bad occured previously and the kernel reused pid from that process.
+     * Small probability, isn't it.
+     */
+    unlink(sun.sun_path);
+    unlink(REQUESTOR_DAEMON_PATH);
+
+    int previous_umask = umask(027);
+    mkdir(REQUESTOR_DAEMON_PATH, 0770);
+    
+    if (bind(fd, (struct sockaddr*)&sun, sizeof(sun)) < 0) {
+        PLOGE("daemon bind");
+        goto err;
+    }
+    
+    umask(previous_umask);
+
+    if (listen(fd, 10) < 0) {
+        PLOGE("daemon listen");
+        goto err;
+    }
+    
+    int client;
+    while ((client = accept(fd, NULL, NULL)) > 0) {
+        // dup/close?
+        LOGD("got client");
+        if (fork() == 0) {
+            close(fd);
+            return daemon_accept(client);
+        }
+        else {
+            close(client);
+        }
+    }
+
+    LOGE("daemon exiting");
+err:
+    close(fd);
+    return -1;
+}
+
+static void pump(int input, int output) {
+    char buf[4096];
+    int len;
+    while ((len = read(input, buf, 4096)) > 0) {
+        LOGE("len: %d", len);
+        write(output, buf, len);
+    }
+    LOGE("len: %d", len);
+    close(input);
+    close(output);
+}
+
+static void* pump_thread(void* data) {
+    int* files = (int*)data;
+    int input = files[0];
+    int output = files[1];
+    pump(input, output);
+    free(data);
+    return NULL;
+}
+
+static int connect_daemon(int argc, char *argv[]) {
+    char outfile[PATH_MAX];
+    char infile[PATH_MAX];
+    sprintf(outfile, "%s/%d.stdout", REQUESTOR_DAEMON_PATH, getpid());
+    sprintf(infile, "%s/%d.stdin", REQUESTOR_DAEMON_PATH, getpid());
+    LOGD("client infile: %s", infile);
+    LOGD("client outfile: %s", outfile);
+    unlink(infile);
+    unlink(outfile);
+
+    if (mkfifo(outfile, 0660) != 0) {
+        PLOGE("mkfifo %s", outfile);
+        exit(-1);
+    }
+    if (mkfifo(infile, 0660) != 0) {
+        PLOGE("mkfifo %s", infile);
+        exit(-1);
+    }
+
+    struct sockaddr_un sun;
+
+    int socketfd = socket(AF_LOCAL, SOCK_STREAM, 0);
+    if (socketfd < 0) {
+        PLOGE("socket");
+        exit(-1);
+    }
+
+    memset(&sun, 0, sizeof(sun));
+    sun.sun_family = AF_LOCAL;
+    sprintf(sun.sun_path, "%s/server", REQUESTOR_DAEMON_PATH);
+    
+    if (0 != connect(socketfd, (struct sockaddr*)&sun, sizeof(sun))) {
+        PLOGE("connect");
+        exit(-1);
+    }
+    
+    LOGD("client is: %d", getpid());
+    write_int(socketfd, getpid());
+    write_int(socketfd, getuid());
+    write_int(socketfd, getppid());
+    write_int(socketfd, argc);
+
+    int i;
+    for (i < 0; i < argc; i++) {
+        write_string(socketfd, argv[i]);
+    }
+  
+    int outfd = open(outfile, O_RDONLY);
+    if (outfd <= 0) {
+        PLOGE("outfd");
+        exit(-1);
+    }
+    int infd = open(infile, O_WRONLY);
+    if (infd <= 0) {
+        PLOGE("infd");
+        exit(-1);
+    }
+
+    pthread_t input_reader;
+    int* files = (int*)malloc(sizeof(int) * 2);
+    files[0] = STDIN_FILENO;
+    files[1] = infd;
+    // pthread_create(&input_reader, NULL, pump_thread, files);
+
+    pump(outfd, STDOUT_FILENO);
+
+    int code = read_int(socketfd);
+    LOGD("daemon client done %d", code);
+    return code;
+}
+
 int main(int argc, char *argv[]) {
+    // start up in daemon mode if prompted
+    if (argc == 2 && strcmp(argv[1], "--daemon") == 0) {
+        return run_daemon();
+    }
+
+    struct stat st;
+    // Exec /superuser/su if it exists, and we are not root.
+    // This is necessary for 4.3, as /system is now nosuid.
+    char* superuser_binary = "/superuser/su";
+    if (geteuid() != AID_ROOT && getuid() != AID_ROOT && stat(superuser_binary, &st) == 0 && strcmp(argv[0], superuser_binary) != 0) {
+        LOGD("execing /superuser/su daemon");
+        LOGE("currently (%ld, %ld)", getuid(), geteuid());
+        argv[argc - 1] = superuser_binary;
+        sleep(2);
+        return execvp(superuser_binary, argv);
+    }
+    else if (strcmp(argv[0], superuser_binary) == 0 && !is_daemon) {
+        // if this is the daemon client, connect to the deamon and send what to do.
+        LOGD("starting daemon client");
+        return connect_daemon(argc, argv);
+    }
+
     // Sanitize all secure environment variables (from linker_environ.c in AOSP linker).
     /* The same list than GLibc at this point */
     static const char* const unsec_vars[] = {
@@ -659,7 +963,6 @@ int main(int argc, char *argv[]) {
             .base_path = REQUESTOR_DATA_PATH REQUESTOR
         },
     };
-    struct stat st;
     int c, socket_serv_fd, fd;
     char buf[64], *result;
     policy_t dballow;
