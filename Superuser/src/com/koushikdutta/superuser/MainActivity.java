ce49752266dd36eca48402a1bd75d447e75b5bb0
BILLyTheLiTTle <littleprog@gmail.com>
2013-09-20 22:17:12 +0300

    Fix several compiler warnings
    Change-Id: I477f7f0dc37cc82028a3f1fa280dad1977f20efc

--- Superuser/src/com/koushikdutta/superuser/MainActivity.java
+++ Superuser/src/com/koushikdutta/superuser/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends BetterListActivity {
     public PolicyFragmentInternal getFragment() {
         return (PolicyFragmentInternal)super.getFragment();
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater mi = new MenuInflater(this);
@@ -64,10 +64,10 @@ public class MainActivity extends BetterListActivity {
                 return true;
             }
         });
-        
+
         return super.onCreateOptionsMenu(menu);
     }
-    
+
     File extractSu() throws IOException, InterruptedException {
         String arch = "armeabi";
         if (System.getProperty("os.arch").contains("x86") || System.getProperty("os.arch").contains("i686") || System.getProperty("os.arch").contains("i386"))
@@ -101,7 +101,7 @@ public class MainActivity extends BetterListActivity {
                 in.close();
                 zf.close();
             }
-            
+
             public void run() {
                 try {
                     File zip = getFileStreamPath("superuser.zip");
@@ -159,7 +159,7 @@ public class MainActivity extends BetterListActivity {
             }
         }.start();
     }
-    
+
     void doSystemInstall() {
         final ProgressDialog dlg = new ProgressDialog(this);
         dlg.setTitle(R.string.installing);
@@ -204,7 +204,7 @@ public class MainActivity extends BetterListActivity {
                         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                         builder.setPositiveButton(android.R.string.ok, null);
                         builder.setTitle(R.string.install);
-                        
+
                         if (error) {
                             builder.setMessage(R.string.install_error);
                         }
@@ -217,7 +217,7 @@ public class MainActivity extends BetterListActivity {
             };
         }.start();
     }
-    
+
     void doInstall() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.install);
@@ -239,11 +239,11 @@ public class MainActivity extends BetterListActivity {
         });
         builder.create().show();
     }
-    
+
     private void saveWhatsNew() {
         Settings.setString(this, "whats_new", WHATS_NEW);
     }
-    
+
     // this is intentionally not localized as it will change constantly.
     private static final String WHATS_NEW = "Added support for Android 4.3.";
     protected void doWhatsNew() {
@@ -270,12 +270,12 @@ public class MainActivity extends BetterListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Settings.applyDarkThemeSetting(this, R.style.SuperuserDarkActivity);
         super.onCreate(savedInstanceState);
-        
+
         if (Settings.getBoolean(this, "first_run", true)) {
             saveWhatsNew();
             Settings.setBoolean(this, "first_run", false);
         }
-        
+
         final ProgressDialog dlg = new ProgressDialog(this);
         dlg.setTitle(R.string.superuser);
         dlg.setMessage(getString(R.string.checking_superuser));
