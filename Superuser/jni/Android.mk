e253c4ec205ba737c58a06edb8fd4693a4d41c19
Danny Baumann <dannybaumann@web.de>
2013-09-23 11:13:06 +0200

    Improve logging code
    - Use static version of liblog
    - Reduce debug spam
    
    Change-Id: I9afe7249fc589268ea61419a7cfc724897981e9e

--- Superuser/jni/Android.mk
+++ Superuser/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := su
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_STATIC_LIBRARIES := sqlite3 libcutils libc
+LOCAL_STATIC_LIBRARIES := sqlite3 libcutils libc liblog
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/sqlite3
 LOCAL_SRC_FILES := su/su.c su/activity.c su/db.c su/utils.c su/daemon.c
 include $(BUILD_EXECUTABLE)
