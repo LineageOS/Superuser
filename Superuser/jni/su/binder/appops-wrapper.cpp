6bfb51229d42d0a4df16f10be4247e58036d6ef8
Diogo Ferreira <defer@cyngn.com>
2014-12-16 10:27:31 +0000

    su: Appops integration
    Initial support for an app-ops based allow-chain.
    
    Change-Id: Ie5c6865e1772f7127f10dbd8f68e2fe930fa0407

---
+++ Superuser/jni/su/binder/appops-wrapper.cpp
@@ -0,0 +1,26 @@
+#include <binder/AppOpsManager.h>
+#include <utils/Log.h>
+
+using namespace android;
+
+extern "C" {
+
+int check_appops(int uid, const char *pkgName) {
+    ALOGD("Checking whether app [uid:%d, pkgName: %s] is allowed to be root", uid, pkgName);
+    AppOpsManager *ops = new AppOpsManager();
+
+    int mode = ops->noteOp(61, uid, String16(pkgName));
+
+    switch (mode) {
+        case AppOpsManager::MODE_ALLOWED:
+          ALOGD("Privillege elevation allowed by appops");
+          return 0;
+        default:
+          ALOGD("Privillege elevation denied by appops");
+          return 1;
+    }
+
+    free(ops);
+}
+
+}
